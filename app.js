56a309a5da93ba43fe5eb6992645e5e60863e7ec
mifouts <fouts.mitch15@gmail.com>
2022-07-29 16:35:44 -0400

    another practice problem

--- app.js
+++ app.js
@@ -20,3 +20,36 @@ console.log(fahrenheit)
 let bool = '1' == 1
 
 console.log(bool)
+
+
+let subscribed = false
+let loggedIn = true
+
+if (subscribed === true) {
+    console.log ('show the video')
+}
+
+else if (loggedIn === true) {
+    console.log('tell user to upgrade subscription')
+}
+
+else {
+    console.log('tell user to log into account')
+}
+
+
+let cash = 100
+let price = 50
+let difference = cash - price
+
+if (cash>price) {
+    console.log(`you paid extra - here's ${difference} dollars change`)
+}
+
+else if (cash===price) {
+    console.log("you paid the exact amount, have a nice day!")
+}
+
+else {
+    console.log(`not enough money - you still owe ${difference * -1} dollars`)
+}
