1ef0e50a9dd16e33e827ef4ff30891864be49fd5
mifouts <fouts.mitch15@gmail.com>
2022-07-31 18:59:06 -0400

    practicing loops

--- app.js
+++ app.js
@@ -85,11 +85,33 @@ isStoreOpenn && cashh <= pricee ? console.log('do not give receipt') : console.l
 
 
 // DRY = dont repeat yourself
-let count = 1;
+//let count = 1;
 
 
-while (count <= 100) {
-    console.log(count)
-    count = count + 1
+//while (count <= 100) {
+  //  console.log(count)
+    //count = count + 1
+//}
+
+//for (let i = 0; i < 3; i++) {
+  //  console.log(i)
+//}
+
+for (let i = 1; i <= 20; i++) {
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(`${i} -> Frontend Simplified`)
+       }
+
+   else if (i % 3 === 0) {
+    console.log(`${i} -> Frontend`)
+   }
+
+   else if (i % 5 === 0) {
+    console.log(`${i} -> Simplified`)
+   }
+
+   else {
+    console.log(`${i} -> ${i}`)
+   }
 }
-
