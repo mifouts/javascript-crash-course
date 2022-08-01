67d917ef3b9419780eed4fa96b534d68d0f26c8e
mifouts <fouts.mitch15@gmail.com>
2022-08-01 16:27:35 -0400

    array practice

--- app.js
+++ app.js
@@ -182,6 +182,14 @@ console.log(convertCelsiusToFahrenheit(0))
 
 let grades = ['A+', 'A', 'FAIL']
 
-let goodGrades = grades.filter(element => element !== 'FAIL')
+let goodGrades = []
+
+for (let i = 0; i < grades.length; ++i){
+    if (grades[i] !== 'FAIL'){
+        goodGrades.push(grades[i])
+    }
+    console.log(grades[i]);
+
+}
 
 console.log(goodGrades)
